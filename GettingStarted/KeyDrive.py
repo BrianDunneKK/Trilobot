12d0cf21930ffc6ef6d415b66f8729d0be406ca0
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2024-10-08 22:46:56 +0100

    Improve KeyDrive

--- GettingStarted/KeyDrive.py
+++ GettingStarted/KeyDrive.py
@@ -2,19 +2,23 @@
 
 import time
 import trilobot
-from sshkeyboard import listen_keyboard
+import sshkeyboard
 
 tbot = trilobot.Trilobot()
 exit_now = False
+speed = 9  # Use keys 0..9, where 9 is max speed
 
 
 def press(key):
-    print(f"'{key}' pressed")
+    match key:
+        case "f":
+            print("Forward")
+            tbot.forward()
+        case "b":
+            print("Backward")
+            tbot.backward()
+        case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
+            speed = int(key)
+            print(f"Set speed to '{speed}'")
 
-def release(key):
-    print(f"'{key}' released")
-
-listen_keyboard(
-    on_press=press,
-    on_release=release,
-)
+sshkeyboard.listen_keyboard(on_press=press)
