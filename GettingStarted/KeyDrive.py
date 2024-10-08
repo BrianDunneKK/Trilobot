e2245b13c71c1f8230e83980d42ed72961f0f29f
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2024-10-08 23:09:15 +0100

    Update KeyDrive

--- GettingStarted/KeyDrive.py
+++ GettingStarted/KeyDrive.py
@@ -7,31 +7,31 @@ tbot = trilobot.Trilobot()
 
 def press(key):
     global speed
-    speed = 9  # Use keys 0..9, where 9 is max speed
-    if (key == "s"):
+    speed = 5  # Use keys 0..9, where 9 is max speed
+    if (key == "z"):
         print("Stop")
         tbot.stop()
     elif (key == "c"):
         print("Coast")
         tbot.coast()
-    elif (key == "f"):
+    elif (key == "w"):
         print("Forward")
         tbot.forward(speed/9.0)
-    elif (key == "r"):
-        print("Right (sharp)")
-        tbot.turn_right(speed/9.0)
-    elif (key == "R"):
-        print("Right (curve)")
-        tbot.curve_right(speed/9.0)
-    elif (key == "l"):
-        print("Left (sharp)")
-        tbot.turn_left(speed/9.0)
-    elif (key == "L"):
-        print("Left (curve)")
-        tbot.curve_left(speed/9.0)
-    elif (key == "b"):
+    elif (key == "s"):
         print("Backward")
         tbot.backward(speed/9.0)
+    elif (key == "a"):
+        print("Right (sharp)")
+        tbot.turn_right(speed/9.0)
+    elif (key == "q"):
+        print("Right (curve)")
+        tbot.curve_right(speed/9.0)
+    elif (key == "d"):
+        print("Left (sharp)")
+        tbot.turn_left(speed/9.0)
+    elif (key == "e"):
+        print("Left (curve)")
+        tbot.curve_left(speed/9.0)
     elif (key in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]):
         speed = int(key)
         print(f"Set speed to '{speed}'")
