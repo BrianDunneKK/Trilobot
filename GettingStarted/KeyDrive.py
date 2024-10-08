cf661ce9cd7e870ffee8c07a9db401d10923bdc4
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2024-10-08 22:51:42 +0100

    Improve KeyDrive

--- GettingStarted/KeyDrive.py
+++ GettingStarted/KeyDrive.py
@@ -10,15 +10,14 @@ speed = 9  # Use keys 0..9, where 9 is max speed
 
 
 def press(key):
-    match key:
-        case "f":
-            print("Forward")
-            tbot.forward()
-        case "b":
-            print("Backward")
-            tbot.backward()
-        case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
-            speed = int(key)
-            print(f"Set speed to '{speed}'")
+    if (key == "f"):
+        print("Forward")
+        tbot.forward()
+    elif (key == "b"):
+        print("Backward")
+        tbot.backward()
+    elif (key in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]):
+        speed = int(key)
+        print(f"Set speed to '{speed}'")
 
 sshkeyboard.listen_keyboard(on_press=press)
